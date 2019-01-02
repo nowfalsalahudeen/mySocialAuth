b2cc8671937dc3053153ebbbdc3e9b7c0044ccbf
Nowfal <nowfalsalahudeen@gmail.com>
2019-01-02 15:36:23 +0530

    first commit

---
+++ nssocialauth/src/main/java/com/nowfal/nssocialauth/ToasterMessage.kt
@@ -0,0 +1,12 @@
+package com.nowfal.nssocialauth
+
+import android.content.Context
+import android.widget.Toast
+
+object ToasterMessage {
+    fun s(c: Context, message: String) {
+
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
+
+    }
+}
